d63ca65724b748f267997c6c583835876a84884a
sergiopl97 <sergiopl1997@gmail.com>
2019-11-13 19:01:40 +0100

    .

--- src/com/company/Circuito.java
+++ src/com/company/Circuito.java
@@ -1,18 +1,17 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Circuito {
     Scanner scanner = new Scanner(System.in);
-    void RandomCircuito() {
-        System.out.println("Cuantos circuitos se van a correr?");
-        int circuitos = scanner.nextInt();
-        char[] nombreCircuito = {'A','B','C','D','E','F','G','H','I','J'};
-        char[] nca = new char[circuitos];
-
-        for (int i = 0; i <=circuitos ; i++) {
-            nca[i] = nombreCircuito[(int)(Math.random()*10)];
-            System.out.println(nca[i]);
-            }
+    ArrayList<String> circuito = new ArrayList<String>();
+    void añadirCircuito() {
+        System.out.println("Que circuitos se van a correr?(A,B,C,D,E,F,G,H,I,J) MAXIMO 5 CIRCUITOS");
+        for (int i = 0; i <5 ; i++) {
+            String opcion = scanner.next();
+            circuito.add(opcion);
+        }
      }
 }
